5f566f7d587bf0da6544764b17fa6867337031ec
dreamscriptures-Moon <dreamscriptures-Moon@users.noreply.github.com>
2026-04-25 12:59:35 +0300

    Add GTM and update sitemap/routes

--- app/layout.js
+++ app/layout.js
@@ -54,13 +54,38 @@ const inter = Inter({
   variable: "--font-inter",
 });
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-scroll-behavior="smooth">
       <body className={`${playfair.variable} ${inter.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P4CX4JJB"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         {children}
+
         <Analytics />
         <SpeedInsights />
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P4CX4JJB');
+          `}
+        </Script>
+
+        {/* Existing Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-C8E9Y4L832"
           strategy="afterInteractive"
